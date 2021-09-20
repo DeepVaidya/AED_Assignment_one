8c9d6370a007136557e8637a3511bbdbf96a33fd
Deep Anand Vaidya <vaidya.de@northeastern.edu>
2021-09-19 20:10:18 -0400

    Initial commit

---
+++ AssignmentOne/src/model/PersonalDetails.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author deepv
+ */
+public class PersonalDetails {
+
+    private String name;
+    private String geoData;
+    private String dob;
+    private String teleNos;
+    private String faxNo;
+    private String email;
+    private String ssn;
+    private String medicalRecNum;
+    private String healthPlanNum;
+    private String bankAcc;
+    private String licenseNo;
+    private String vehicleIden;
+    private String deviceIden;
+    private String linkedIn;
+    private String ipAddress;
+    private String biometric;
+    private String facePhoto;
+    private String uniqueIden;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeoData() {
+        return geoData;
+    }
+
+    public void setGeoData(String geoData) {
+        this.geoData = geoData;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTeleNos() {
+        return teleNos;
+    }
+
+    public void setTeleNos(String teleNos) {
+        this.teleNos = teleNos;
+    }
+
+    public String getFaxNo() {
+        return faxNo;
+    }
+
+    public void setFaxNo(String faxNo) {
+        this.faxNo = faxNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRecNum() {
+        return medicalRecNum;
+    }
+
+    public void setMedicalRecNum(String medicalRecNum) {
+        this.medicalRecNum = medicalRecNum;
+    }
+
+    public String getHealthPlanNum() {
+        return healthPlanNum;
+    }
+
+    public void setHealthPlanNum(String healthPlanNum) {
+        this.healthPlanNum = healthPlanNum;
+    }
+
+    public String getBankAcc() {
+        return bankAcc;
+    }
+
+    public void setBankAcc(String bankAcc) {
+        this.bankAcc = bankAcc;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public String getVehicleIden() {
+        return vehicleIden;
+    }
+
+    public void setVehicleIden(String vehicleIden) {
+        this.vehicleIden = vehicleIden;
+    }
+
+    public String getDeviceIden() {
+        return deviceIden;
+    }
+
+    public void setDeviceIden(String deviceIden) {
+        this.deviceIden = deviceIden;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getBiometric() {
+        return biometric;
+    }
+
+    public void setBiometric(String biometric) {
+        this.biometric = biometric;
+    }
+
+    public String getFacePhoto() {
+        return facePhoto;
+    }
+
+    public void setFacePhoto(String facePhoto) {
+        this.facePhoto = facePhoto;
+    }
+
+    public String getUniqueIden() {
+        return uniqueIden;
+    }
+
+    public void setUniqueIden(String uniqueIden) {
+        this.uniqueIden = uniqueIden;
+    }
+
+}
